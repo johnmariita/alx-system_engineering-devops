fb8948f70136a7a50d3043cdcbec0328da6b38fa
johnmariita <johnmariita421@gmail.com>
2024-05-04 01:09:50 +0300

    readme file and the create file puppet manifest

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Puppet manifest to create a file
 
 file {'/tmp/school':
-  path       => '/tmp/school',
-  mode => '0744',
-  owner      => 'www-data',
-  group      => 'www-data',
-  content    => 'I love Puppet',
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
