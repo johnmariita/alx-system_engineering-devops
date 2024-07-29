69722dd643a571809d9f18a970f310bfaf2593ba
johnmariita <johnmariita421@gmail.com>
2024-07-29 23:22:01 +0300

    employees result perfomance

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+python script REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+
+import json
+import sys
+import urllib.request
+
+emp_id = sys.argv[1]
+name = ""
+user_url = "https://jsonplaceholder.typicode.com/users/" + emp_id
+todo_url = user_url + "/todos"
+
+with urllib.request.urlopen(user_url) as resp:
+    details = resp.read().decode('utf-8')
+    details = json.loads(details)
+    name = details['name']
+
+with urllib.request.urlopen(todo_url) as resp:
+    details = resp.read().decode()
+    details = json.loads(details)
+    tasks = []
+    done = 0
+    for task in details:
+        if task['completed']:
+            done += 1
+            tasks.append(task['title'])
+
+    print(f"Employee name {name} is done with tasks({done}/{len(details)}):")
+    for t in tasks:
+        print(f"\t {t}")
